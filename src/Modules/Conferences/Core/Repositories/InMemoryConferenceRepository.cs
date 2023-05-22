96c2ecd5bed05253a15d6455bfb9b5b1a30b86fc
PilarczykM <marcinpilarczyk@me.com>
2023-05-22 16:29:46 +0200

    Create InMemoryConferenceRepository.cs
    Implementation of conference repository, in memory so far.

---
+++ src/Modules/Conferences/Core/Repositories/InMemoryConferenceRepository.cs
@@ -0,0 +1,37 @@
+﻿using Confab.Modules.Conferences.Core.Entities;
+
+namespace Confab.Modules.Conferences.Core.Repositories
+{
+    internal class InMemoryConferenceRepository : IConferenceRepository
+    {
+        // Not thread safe. Use concurrent collections.
+        private readonly List<Conference> _conferences = new();
+
+        public Task AddAsync(Conference conference)
+        {
+            _conferences.Add(conference);
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteAsync(Conference conference)
+        {
+            _conferences.Remove(conference);
+            return Task.CompletedTask;
+        }
+
+        public async Task<IReadOnlyList<Conference>> GetAllAsync()
+        {
+            await Task.CompletedTask;
+            return _conferences;
+        }
+
+        public Task<Conference> GetAsync(Guid id) =>
+            Task.FromResult(_conferences.SingleOrDefault(x => x.Id == id));
+
+        public Task UpdateAsync(Conference conference)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
+

5e2a971e7085d7795d3be4857352683800aa5d87
PilarczykM <marcinpilarczyk@me.com>
2023-05-22 12:28:17 +0200

    Add new exceptions

---
+++ src/Modules/Conferences/Core/Exceptions/HostCanNotBeDeletedException.cs
@@ -0,0 +1,12 @@
+﻿using Confab.Shared.Abstractions.Exceptions;
+
+namespace Confab.Modules.Conferences.Core.Exceptions
+{
+    public class HostCanNotBeDeletedException : ConfabException
+    {
+        public HostCanNotBeDeletedException(Guid id) : base($"Host with ID: '{id}' cannot be deleted.")
+        {
+        }
+    }
+}
+

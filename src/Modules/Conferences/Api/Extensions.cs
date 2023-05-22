82fc043a84ce150481ddf31e2e0366102dedbbfe
PilarczykM <marcinpilarczyk@me.com>
2023-05-22 07:48:46 +0200

    Register project references and extensions

---
+++ src/Modules/Conferences/Api/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: InternalsVisibleTo("Confab.Bootstrapper")]
+namespace Confab.Modules.Conferences.Core
+{
+    internal static class Extensions
+    {
+        public static IServiceCollection AddConferences(this IServiceCollection services)
+        {
+            services.AddCore();
+
+            return services;
+        }
+    }
+}
+

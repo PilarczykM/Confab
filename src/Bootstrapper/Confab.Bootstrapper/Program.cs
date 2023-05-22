15cff4fc0ea79caee364c197c36b0a4a74cdb291
PilarczykM <marcinpilarczyk@me.com>
2023-05-22 06:08:36 +0200

    Update project bootsrapper

--- src/Bootstrapper/Confab.Bootstrapper/Program.cs
+++ src/Bootstrapper/Confab.Bootstrapper/Program.cs
@@ -1,6 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace Confab.Bootstrapper
+{
+    public class Program
+    {
+        public static Task Main(string[] args) => CreateHostBuilder(args).Build().RunAsync();
 
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ src/Bootstrapper/Confab.Bootstrapper/Startup.cs
@@ -0,0 +1,26 @@
+﻿namespace Confab.Bootstrapper
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapGet("/", context => context.Response.WriteAsync("Confab API!"));
+            });
+        }
+    }
+}

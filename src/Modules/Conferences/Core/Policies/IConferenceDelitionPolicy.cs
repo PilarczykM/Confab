3f19b42131746655b33c63b9b1be1744be4f4f21
PilarczykM <marcinpilarczyk@me.com>
2023-05-22 12:26:45 +0200

    Create host and conference basic policy

---
+++ src/Modules/Conferences/Core/Policies/IConferenceDelitionPolicy.cs
@@ -0,0 +1,10 @@
+﻿using Confab.Modules.Conferences.Core.Entities;
+
+namespace Confab.Modules.Conferences.Core.Policies
+{
+    internal interface IConferenceDelitionPolicy
+    {
+        Task<bool> CanDeleteAsync(Conference conference);
+    }
+}
+

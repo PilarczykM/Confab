843bd3efbfd9d743a128c9265ad1dfe7b36f7a95
PilarczykM <marcinpilarczyk@me.com>
2023-05-23 06:41:13 +0200

    Create PostgresOptions.cs
    Postgress connection string option

---
+++ src/Shared/Confab.Shared.Infrastructure/Postgres/PostgresOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Confab.Shared.Infrastructure.Postgres
+{
+    internal class PostgresOptions
+    {
+        public const string Postgres = "Postgres";
+        public string ConnectionStrings { get; set; } = string.Empty;
+    }
+}
+

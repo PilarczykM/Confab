709a6eef16a7a2aa581bb3ad9f3b76418b50f9ee
PilarczykM <marcinpilarczyk@me.com>
2023-05-23 12:28:49 +0200

    Move registraction to ConferencesModule
    ConferencesModule is responsible for registration services and middlewares.

---
+++ src/Modules/Conferences/Api/ConferencesModule.cs
@@ -0,0 +1,26 @@
+﻿using Confab.Modules.Conferences.Core;
+using Confab.Shared.Abstractions.Module;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Confab.Modules.Conferences.Api
+{
+    public class ConferencesModule : IModule
+    {
+        public const string BasePath = "conferences-module";
+
+        public string Name => "Conferences";
+
+        public string Path => BasePath;
+
+        public void Register(IServiceCollection services)
+        {
+            services.AddCore();
+        }
+
+        public void Use(IApplicationBuilder app)
+        {
+        }
+    }
+}
+

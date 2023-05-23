4ee393a765d70e6b4abb991d4a83de154e390c75
PilarczykM <marcinpilarczyk@me.com>
2023-05-23 16:38:08 +0200

    Create Extensions.cs
    Register all services

---
+++ src/Modules/Speakers/Core/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using Confab.Modules.Speakers.Core.DAL;
+using Confab.Modules.Speakers.Core.DAL.Repositories;
+using Confab.Modules.Speakers.Core.Services;
+using Confab.Shared.Infrastructure.Postgres;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Confab.Modules.Speakers.Core
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddCore(this IServiceCollection services)
+        {
+            services.AddScoped<ISpeakersService, SpeakersService>();
+            services.AddScoped<ISpeakersRepository, SpeakersRepository>();
+            services.AddPostgres<SpeakerDbContext>();
+
+            return services;
+        }
+    }
+}
+

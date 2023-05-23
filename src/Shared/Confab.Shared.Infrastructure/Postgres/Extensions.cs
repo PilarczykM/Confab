63645eee301dc88f6433c84c9c1d7f90f76dd6f3
PilarczykM <marcinpilarczyk@me.com>
2023-05-23 06:42:50 +0200

    Create Extensions.cs
    Retrive postgres connection stirng

---
+++ src/Shared/Confab.Shared.Infrastructure/Postgres/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Confab.Shared.Infrastructure.Postgres
+{
+    public static class Extensions
+    {
+        internal static IServiceCollection AddPostgres(this IServiceCollection services)
+        {
+            var options = services.GetOptions<PostgresOptions>(PostgresOptions.Postgres);
+            services.AddSingleton(options);
+
+            return services;
+        }
+    }
+}
+

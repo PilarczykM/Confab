e9a557cf00acebc564ea63569c5e63db13441e8e
PilarczykM <marcinpilarczyk@me.com>
2023-05-23 12:30:48 +0200

    Use module loader on startup

--- src/Bootstrapper/Confab.Bootstrapper/Startup.cs
+++ src/Bootstrapper/Confab.Bootstrapper/Startup.cs
@@ -1,20 +1,44 @@
-﻿using Confab.Modules.Conferences.Api;
+﻿using System.Reflection;
+using Confab.Shared.Abstractions.Module;
 using Confab.Shared.Infrastructure;
 
 namespace Confab.Bootstrapper
 {
     public class Startup
     {
+        private readonly IList<Assembly> _assemblies;
+        private readonly IList<IModule> _modules;
+
+        public Startup()
+        {
+            _assemblies = ModuleLoader.LoadAssemblies();
+            _modules = ModuleLoader.LoadModules(_assemblies);
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddInfrastructure();
-            services.AddConferences();
+            services.AddSharedInfrastructure();
+
+            foreach (var module in _modules)
+            {
+                module.Register(services);
+            }
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSharedInfrastructure();
 
-            app.UseInfrastructure();
+            foreach (var module in _modules)
+            {
+                module.Use(app);
+            }
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapGet("/", context => context.Response.WriteAsync("Confab API!"));
+            });
         }
     }
 }

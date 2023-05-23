7b8bd4c8022e79d3db267631cb8f86f33ae9e4e0
PilarczykM <marcinpilarczyk@me.com>
2023-05-23 16:34:29 +0200

    Create ISpeakersService.cs
    Prepare ISpeakersService interface

---
+++ src/Modules/Speakers/Core/Services/ISpeakersService.cs
@@ -0,0 +1,12 @@
+﻿using Confab.Modules.Speakers.Core.DTO;
+
+namespace Confab.Modules.Speakers.Core.Services
+{
+    public interface ISpeakersService
+    {
+        Task<IEnumerable<SpeakerDto>> BrowseAsync();
+        Task<SpeakerDto> GetAsync(Guid id);
+        Task CreateAsync(SpeakerDto speaker);
+        Task UpdateAsync(SpeakerDto speaker);
+    }
+}

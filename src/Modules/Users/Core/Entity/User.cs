0dc17c8c16132346c580ab63be98314c6ed6940b
PilarczykM <marcinpilarczyk@me.com>
2023-05-25 14:53:36 +0200

    Update User.cs
    Change to internal

--- src/Modules/Users/Core/Entity/User.cs
+++ src/Modules/Users/Core/Entity/User.cs
@@ -1,6 +1,6 @@
 ﻿namespace Confab.Modules.Users.Core.Entity
 {
-    internal class User
+    public class User
     {
         public Guid Id { get; set; }
         public string Email { get; set; }

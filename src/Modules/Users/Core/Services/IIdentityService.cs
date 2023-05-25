9a041b1a6996cda402ab9a30f1b30c4f50b02d45
PilarczykM <marcinpilarczyk@me.com>
2023-05-25 14:28:00 +0200

    Create IIdentityService.cs

---
+++ src/Modules/Users/Core/Services/IIdentityService.cs
@@ -0,0 +1,13 @@
+﻿using Confab.Modules.Users.Core.DTO;
+using Confab.Shared.Abstractions.Auth;
+
+namespace Confab.Modules.Users.Core.Services
+{
+    public interface IIdentityService
+    {
+        Task<AccountDto> GetAsync(Guid id);
+        Task SignUpAsync(SignUpDto dto);
+        Task<JsonWebToken> SignInAsync(SignInDto dto);
+    }
+}
+

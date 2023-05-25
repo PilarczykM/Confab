171a5a9fb7c6eaa6066701da9931a668393b17ae
PilarczykM <marcinpilarczyk@me.com>
2023-05-25 14:28:43 +0200

    Create Extensions.cs
    Register services

---
+++ src/Modules/Users/Core/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using Confab.Modules.Users.Core.DAL;
+using Confab.Modules.Users.Core.DAL.Repositories;
+using Confab.Modules.Users.Core.Entity;
+using Confab.Modules.Users.Core.Repositories;
+using Confab.Modules.Users.Core.Services;
+using Confab.Shared.Infrastructure.Postgres;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Confab.Modules.Users.Core
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddCore(this IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddSingleton<IPasswordHasher<User>, PasswordHasher<User>>();
+            services.AddTransient<IIdentityService, IdentityService>();
+            services.AddPostgres<UsersDbContext>();
+
+            return services;
+        }
+    }
+}
+

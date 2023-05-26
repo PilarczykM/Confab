58abaf3bc9a812960022dd279c3c279611e34b1e
PilarczykM <marcinpilarczyk@me.com>
2023-05-26 12:03:55 +0200

    Use new attribute

--- src/Modules/Speakers/Api/Controllers/BaseController.cs
+++ src/Modules/Speakers/Api/Controllers/BaseController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Confab.Shared.Infrastructure.Api;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Confab.Modules.Speakers.Api.Controllers
 {
     [ApiController]
     [Route(SpeakersModule.BasePath + "/[controller]")]
+    [ProducesDefaultContentType]
     internal abstract class BaseController : ControllerBase
     {
         protected ActionResult<T> OkOrNotFound<T>(T model)

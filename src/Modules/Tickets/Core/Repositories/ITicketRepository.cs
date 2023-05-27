5c6ab8c9bc4d95e2885dab1d21225a70eed04728
PilarczykM <marcinpilarczyk@me.com>
2023-05-27 06:32:40 +0200

    Add repositories for entities

---
+++ src/Modules/Tickets/Core/Repositories/ITicketRepository.cs
@@ -0,0 +1,16 @@
+﻿using Confab.Modules.Tickets.Core.Entities;
+
+namespace Confab.Modules.Tickets.Core.Repositories
+{
+    internal interface ITicketRepository
+    {
+        Task<Ticket> GetAsync(Guid conferenceId, Guid userId);
+        Task<int> CounForConferenceAsync(Guid conferenceId);
+        Task<IReadOnlyList<Ticket>> GetForUserAsync(Guid userId);
+        Task AddAsync(Ticket ticket);
+        Task UpdateAsync(Ticket ticket);
+        Task DeleteAsync(Ticket ticket);
+        Task AddManyAsync(IEnumerable<Ticket> ticket);
+    }
+}
+

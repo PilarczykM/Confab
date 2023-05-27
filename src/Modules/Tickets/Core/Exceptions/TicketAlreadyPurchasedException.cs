69361c9ef5b0bbd35d8e45199ec03c596bedebb0
PilarczykM <marcinpilarczyk@me.com>
2023-05-27 06:33:04 +0200

    Create TicketAlreadyPurchasedException.cs

---
+++ src/Modules/Tickets/Core/Exceptions/TicketAlreadyPurchasedException.cs
@@ -0,0 +1,19 @@
+﻿using Confab.Shared.Abstractions.Exceptions;
+
+namespace Confab.Modules.Tickets.Core.Exceptions
+{
+    internal class TicketAlreadyPurchasedException : ConfabException
+    {
+        private Guid conferenceId;
+        private readonly Guid userId;
+        private Guid value;
+
+        public TicketAlreadyPurchasedException(Guid conferenceId, Guid userId) : base($"")
+        {
+            this.conferenceId = conferenceId;
+            this.userId = userId;
+        }
+
+    }
+}
+

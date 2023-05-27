93a4ba168b44ae5b889486c78fbd7f2d42f09653
PilarczykM <marcinpilarczyk@me.com>
2023-05-27 07:27:11 +0200

    Create Extensions.cs
    Register services

---
+++ src/Modules/Tickets/Core/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using Confab.Modules.Tickets.Core.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Confab.Modules.Tickets.Core
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddCore(this IServiceCollection services)
+        {
+            services.AddScoped<ITicketService, TicketService>();
+            services.AddScoped<ITicketSaleService, TicketSaleService>();
+
+            services.AddSingleton<ITicketGenereator, TicketGenereator>();
+            return services;
+        }
+    }
+}
+

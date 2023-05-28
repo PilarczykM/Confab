66b537bc9eaf1f67ff2c5941083609dacda3b07b
PilarczykM <marcinpilarczyk@me.com>
2023-05-28 06:56:33 +0200

    Create IEventDispatcher.cs
    Event dispatcher

---
+++ src/Shared/Confab.Shared.Abstractions/Events/IEventDispatcher.cs
@@ -0,0 +1,8 @@
+﻿namespace Confab.Shared.Abstractions.Events
+{
+    public interface IEventDispatcher
+    {
+        Task PublicAsync<TEvent>(TEvent @event) where TEvent : class, IEvent;
+    }
+}
+

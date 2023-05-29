27145fe06d4ad67ee2e0132c2205ab927c2654cf
PilarczykM <marcinpilarczyk@me.com>
2023-05-29 18:26:04 +0200

    Create Extensions.cs

---
+++ src/Shared/Confab.Shared.Infrastructure/Messaging/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Confab.Shared.Abstractions.Messaging;
+using Confab.Shared.Infrastructure.Messaging.Brokers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Confab.Shared.Infrastructure.Messaging
+{
+    internal static class Extensions
+    {
+        public static IServiceCollection AddMessaging(this IServiceCollection services)
+        {
+            services.AddSingleton<IMessageBroker, InMemoryMessageBroker>();
+
+            return services;
+        }
+    }
+}
+

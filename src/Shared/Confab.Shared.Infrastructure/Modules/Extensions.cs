f0dc2fbfa5b98e6d01fd0802e97aca8e1f8a632e
PilarczykM <marcinpilarczyk@me.com>
2023-05-29 16:04:07 +0200

    Update Extensions.cs

--- src/Shared/Confab.Shared.Infrastructure/Modules/Extensions.cs
+++ src/Shared/Confab.Shared.Infrastructure/Modules/Extensions.cs
@@ -1,4 +1,6 @@
-﻿using Confab.Shared.Abstractions.Module;
+﻿using System.Reflection;
+using Confab.Shared.Abstractions.Events;
+using Confab.Shared.Abstractions.Module;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
@@ -66,5 +68,51 @@ namespace Confab.Shared.Infrastructure.Modules
                         );
                 }
             );
+
+        internal static IServiceCollection AddModuleRequests(
+            this IServiceCollection services,
+            IList<Assembly> assemblies
+        )
+        {
+            services.AddModuleRegistry(assemblies);
+            services.AddSingleton<IModuleClient, ModuleClient>();
+            services.AddSingleton<IModuleSerializer, JsonModuleSerializer>();
+
+            return services;
+        }
+
+        private static void AddModuleRegistry(
+            this IServiceCollection services,
+            IEnumerable<Assembly> assemblies
+        )
+        {
+            var registry = new ModuleRegistry();
+
+            var types = assemblies.SelectMany(x => x.GetTypes()).ToArray();
+            var eventTypes = types
+                .Where(x => x.IsClass && typeof(IEvent).IsAssignableFrom(x))
+                .ToArray();
+
+            services.AddSingleton<IModuleRegistry>(sp =>
+            {
+                var eventDispatcher = sp.GetRequiredService<IEventDispatcher>();
+                var eventDispatcherType = eventDispatcher.GetType();
+
+                foreach (var type in eventTypes)
+                {
+                    registry.AddBroadcastAction(
+                        type,
+                        @event =>
+                            (Task)
+                                eventDispatcherType
+                                    .GetMethod(nameof(eventDispatcher.PublicAsync))
+                                    ?.MakeGenericMethod(type)
+                                    .Invoke(eventDispatcher, new[] { @event })
+                    );
+                }
+
+                return registry;
+            });
+        }
     }
 }

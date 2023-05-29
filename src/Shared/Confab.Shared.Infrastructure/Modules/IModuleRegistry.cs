4ee45dbafd42257bd474d9b5195de91e3ad646ff
PilarczykM <marcinpilarczyk@me.com>
2023-05-29 16:01:21 +0200

    Create IModuleRegistry.cs

---
+++ src/Shared/Confab.Shared.Infrastructure/Modules/IModuleRegistry.cs
@@ -0,0 +1,8 @@
+﻿namespace Confab.Shared.Infrastructure.Modules
+{
+    public interface IModuleRegistry
+    {
+        IEnumerable<ModuleBroadcastRegistration> GetBroadcastRegistrations(string key);
+        void AddBroadcastAction(Type requestType, Func<object, Task> action);
+    }
+}

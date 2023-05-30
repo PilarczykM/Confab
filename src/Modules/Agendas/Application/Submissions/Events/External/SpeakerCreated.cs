f80cd108532bc46fb012a61fb35e69a8e48fe36f
PilarczykM <marcinpilarczyk@me.com>
2023-05-30 13:50:59 +0200

    Create SpeakerCreated.cs

---
+++ src/Modules/Agendas/Application/Submissions/Events/External/SpeakerCreated.cs
@@ -0,0 +1,7 @@
+﻿using Confab.Shared.Abstractions.Events;
+
+namespace Confab.Modules.Agendas.Application.Submissions.Events.External
+{
+    public record SpeakerCreated(Guid id, string FullName) : IEvent;
+}
+

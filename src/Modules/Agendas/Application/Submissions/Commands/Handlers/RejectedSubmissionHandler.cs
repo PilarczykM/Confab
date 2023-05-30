6724bc30a441107daa29960daa77fde3aeb9c234
PilarczykM <marcinpilarczyk@me.com>
2023-05-30 15:24:20 +0200

    Create RejectedSubmissionHandler.cs

---
+++ src/Modules/Agendas/Application/Submissions/Commands/Handlers/RejectedSubmissionHandler.cs
@@ -0,0 +1,26 @@
+﻿using Confab.Modules.Agendas.Application.Submissions.Exceptions;
+using Confab.Modules.Agendas.Domain.Submissions.Repositories;
+using Confab.Shared.Abstractions.Commands;
+
+namespace Confab.Modules.Agendas.Application.Submissions.Commands.Handlers
+{
+    public sealed class RejectedSubmissionHandler : ICommandHandler<RejectedSubmission>
+    {
+        private readonly ISubmissionRepository _submissionRepository;
+
+        public RejectedSubmissionHandler(ISubmissionRepository submissionRepository)
+        {
+            _submissionRepository = submissionRepository;
+        }
+
+        public async Task HandleAsync(RejectedSubmission command)
+        {
+            var submission =
+                await _submissionRepository.GetAsync(command.Id)
+                ?? throw new SubmissionNotFoundException(command.Id);
+
+            submission.Rejected();
+            await _submissionRepository.UpdateAsync(submission);
+        }
+    }
+}

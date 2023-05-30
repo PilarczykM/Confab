f448a96491d261f1c08ed06e59f6acc388586a3e
PilarczykM <marcinpilarczyk@me.com>
2023-05-30 16:25:55 +0200

    Create SpeakerDto.cs

---
+++ src/Modules/Agendas/Application/Submissions/DTO/SpeakerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Confab.Modules.Agendas.Application.Submissions.DTO
+{
+    public class SpeakerDto
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+    }
+}
+

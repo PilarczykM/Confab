5e0554a764cb12e5a469263347186b7f1f692e7a
PilarczykM <marcinpilarczyk@me.com>
2023-05-30 10:52:03 +0200

    Create HomeController.cs

---
+++ src/Modules/Agendas/Api/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Confab.Modules.Conferences.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Confab.Modules.Agendas.Api.Controllers
+{
+    [Route(AgendasModule.BasePath)]
+    internal class HomeController : BaseController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => "Agendas Api";
+    }
+}

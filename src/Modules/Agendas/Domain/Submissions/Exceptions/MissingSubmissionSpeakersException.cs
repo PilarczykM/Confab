a23705c6306ff9968f770be077a6c430b93a3a27
PilarczykM <marcinpilarczyk@me.com>
2023-05-30 13:12:54 +0200

    Exceptions
    Prepare exceptions for entities

---
+++ src/Modules/Agendas/Domain/Submissions/Exceptions/MissingSubmissionSpeakersException.cs
@@ -0,0 +1,15 @@
+﻿using Confab.Shared.Abstractions.Exceptions;
+
+namespace Confab.Modules.Agendas.Domain.Submissions.Exceptions
+{
+    public class MissingSubmissionSpeakersException : ConfabException
+    {
+        public MissingSubmissionSpeakersException(Guid id)
+            : base($"Submission with ID: '{id}' has missing")
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; }
+    }
+}

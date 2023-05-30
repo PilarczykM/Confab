6a41d31ba83cfcec9f7e8508a48569e714d6671d
PilarczykM <marcinpilarczyk@me.com>
2023-05-30 15:22:46 +0200

    Create CommandDispatcher.cs

---
+++ src/Shared/Confab.Shared.Infrastructure/Commands/CommandDispatcher.cs
@@ -0,0 +1,28 @@
+﻿using Confab.Shared.Abstractions.Commands;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Confab.Shared.Infrastructure.Commands
+{
+    public class CommandDispatcher : ICommandDispatcher
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public CommandDispatcher(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        async Task ICommandDispatcher.SendAsync<TCommand>(TCommand command)
+        {
+            if (command is null)
+            {
+                return;
+            }
+
+            using var scope = _serviceProvider.CreateScope();
+
+            var handler = scope.ServiceProvider.GetRequiredService<ICommandHandler<TCommand>>();
+            await handler.HandleAsync(command);
+        }
+    }
+}

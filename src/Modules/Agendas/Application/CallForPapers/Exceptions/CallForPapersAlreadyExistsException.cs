acdabe21810f0c08b1691df29b52051836339bc7
PilarczykM <marcinpilarczyk@me.com>
2023-06-01 16:46:01 +0200

    Create CallForPapersAlreadyExistsException.cs

---
+++ src/Modules/Agendas/Application/CallForPapers/Exceptions/CallForPapersAlreadyExistsException.cs
@@ -0,0 +1,15 @@
+﻿using Confab.Shared.Abstractions.Exceptions;
+
+namespace Confab.Modules.Agendas.Application.CallForPapers.Exceptions
+{
+    public class CallForPapersAlreadyExistsException : ConfabException
+    {
+        public CallForPapersAlreadyExistsException(Guid conferenceId)
+            : base($"Conference with ID: '{conferenceId}' already defined CFP.")
+        {
+            ConferenceId = conferenceId;
+        }
+
+        public Guid ConferenceId { get; }
+    }
+}

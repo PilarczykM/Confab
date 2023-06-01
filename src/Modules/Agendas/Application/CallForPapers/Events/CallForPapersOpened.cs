ddf6a4361b0ec53dcbc86d8daccdc5a8d98fd580
PilarczykM <marcinpilarczyk@me.com>
2023-06-01 16:45:52 +0200

    Create CallForPapersOpened.cs

---
+++ src/Modules/Agendas/Application/CallForPapers/Events/CallForPapersOpened.cs
@@ -0,0 +1,8 @@
+﻿using Confab.Shared.Abstractions.Events;
+using Confab.Shared.Abstractions.Kernel.Types;
+
+namespace Confab.Modules.Agendas.Application.CallForPapers.Events
+{
+    internal record CallForPapersOpened(ConferenceId ConferenceId, DateTime From, DateTime To)
+        : IEvent;
+}

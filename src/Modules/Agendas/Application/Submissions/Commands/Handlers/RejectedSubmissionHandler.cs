dcb94276e078d7b0d6daf596d03a99378afb17cf
PilarczykM <marcinpilarczyk@me.com>
2023-06-01 15:22:24 +0200

    Update RejectedSubmissionHandler.cs

--- src/Modules/Agendas/Application/Submissions/Commands/Handlers/RejectedSubmissionHandler.cs
+++ src/Modules/Agendas/Application/Submissions/Commands/Handlers/RejectedSubmissionHandler.cs
@@ -1,7 +1,9 @@
-﻿using Confab.Modules.Agendas.Application.Submissions.Exceptions;
+﻿using Confab.Modules.Agendas.Application.Services;
+using Confab.Modules.Agendas.Application.Submissions.Exceptions;
 using Confab.Modules.Agendas.Domain.Submissions.Repositories;
 using Confab.Shared.Abstractions.Commands;
 using Confab.Shared.Abstractions.Kernel;
+using Confab.Shared.Abstractions.Messaging;
 
 namespace Confab.Modules.Agendas.Application.Submissions.Commands.Handlers
 {
@@ -9,6 +11,8 @@ namespace Confab.Modules.Agendas.Application.Submissions.Commands.Handlers
     {
         private readonly ISubmissionRepository _submissionRepository;
         private readonly IDomainEventDispatcher _domainEventDispatcher;
+        private readonly IEventMapper _eventMapper;
+        private readonly IMessageBroker _messageBroker;
 
         public RejectedSubmissionHandler(
             ISubmissionRepository submissionRepository,
@@ -28,6 +32,9 @@ namespace Confab.Modules.Agendas.Application.Submissions.Commands.Handlers
             submission.Rejected();
             await _submissionRepository.UpdateAsync(submission);
             await _domainEventDispatcher.DispatchAsync(submission.Events.ToArray());
+
+            var integrationEvents = _eventMapper.MapAll(submission.Events);
+            await _messageBroker.PublishAsync(integrationEvents.ToArray());
         }
     }
 }

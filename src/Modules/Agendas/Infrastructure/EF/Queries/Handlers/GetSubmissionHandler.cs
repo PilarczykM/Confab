de3b171e0beb16ff149e3bebdeae0b43fdd58358
PilarczykM <marcinpilarczyk@me.com>
2023-06-01 17:41:56 +0200

    Update GetSubmissionHandler.cs

--- src/Modules/Agendas/Infrastructure/EF/Queries/Handlers/GetSubmissionHandler.cs
+++ src/Modules/Agendas/Infrastructure/EF/Queries/Handlers/GetSubmissionHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Confab.Modules.Agendas.Application.Submissions.DTO;
 using Confab.Modules.Agendas.Application.Submissions.Queries;
 using Confab.Modules.Agendas.Domain.Submissions.Entities;
+using Confab.Modules.Agendas.Infrastructure.EF.Mappings;
 using Confab.Shared.Abstractions.Queries;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,21 +19,7 @@ namespace Confab.Modules.Agendas.Infrastructure.EF.Queries.Handlers
             .AsNoTracking()
             .Where(x => x.Id.Equals(query.Id))
             .Include(x => x.Speakers)
-            .Select(x => new SubmissionDto
-            {
-                Id = x.Id,
-                Title = x.Title,
-                Description = x.Description,
-                ConferenceId = x.ConferenceId,
-                Level = x.Level,
-                Status = x.Status,
-                Tags = x.Tags,
-                Speakers = x.Speakers.Select(s => new SpeakerDto
-                {
-                    FullName = s.FullName,
-                    Id = s.Id
-                })
-            }).SingleOrDefaultAsync();
+            .Select(x => x.AsDto()).SingleOrDefaultAsync();
 
     }
 }

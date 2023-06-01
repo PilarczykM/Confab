eea95002e7ce3f03d0fc3fd75c194cfaed21200d
PilarczykM <marcinpilarczyk@me.com>
2023-06-01 17:42:00 +0200

    Create CallForPapersExtension.cs

---
+++ src/Modules/Agendas/Infrastructure/EF/Mappings/CallForPapersExtension.cs
@@ -0,0 +1,19 @@
+﻿using Confab.Modules.Agendas.Application.CallForPapers.DTO;
+using Confab.Modules.Agendas.Domain.CallForPapers.Entities;
+
+namespace Confab.Modules.Agendas.Infrastructure.EF.Mappings
+{
+    internal static class CallForPapersExtension
+    {
+        public static CallForPapersDto AsDto(this CallForPapers callForPapers)
+            => new()
+            {
+                Id = callForPapers.Id,
+                ConferenceId = callForPapers.ConferenceId,
+                From = callForPapers.From,
+                To = callForPapers.To,
+                IsOpened = callForPapers.IsOpened
+            };
+    }
+}
+

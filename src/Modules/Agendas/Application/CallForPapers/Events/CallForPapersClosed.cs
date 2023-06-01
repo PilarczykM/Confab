db61f1291c3b5df3bd641da97577cf118bbf4033
PilarczykM <marcinpilarczyk@me.com>
2023-06-01 16:45:58 +0200

    Create CallForPapersClosed.cs

---
+++ src/Modules/Agendas/Application/CallForPapers/Events/CallForPapersClosed.cs
@@ -0,0 +1,7 @@
+﻿using Confab.Shared.Abstractions.Events;
+using Confab.Shared.Abstractions.Kernel.Types;
+
+namespace Confab.Modules.Agendas.Application.CallForPapers.Events
+{
+    internal record CallForPapersClosed(ConferenceId ConferenceId) : IEvent;
+}

00b0bdd3deab413ee16156ace00c03b1e5ca5bdf
PilarczykM <marcinpilarczyk@me.com>
2023-06-01 16:46:05 +0200

    Create OpenCallForPapersHandler.cs

---
+++ src/Modules/Agendas/Application/CallForPapers/Commands/Handlers/OpenCallForPapersHandler.cs
@@ -0,0 +1,40 @@
+﻿using Confab.Modules.Agendas.Application.CallForPapers.Events;
+using Confab.Modules.Agendas.Application.CallForPapers.Exceptions;
+using Confab.Modules.Agendas.Domain.CallForPapers.Repositories;
+using Confab.Shared.Abstractions.Commands;
+using Confab.Shared.Abstractions.Messaging;
+
+namespace Confab.Modules.Agendas.Application.CallForPapers.Commands.Handlers
+{
+    public class OpenCallForPapersHandler : ICommandHandler<OpenCallForPapers>
+    {
+        private readonly ICallForPapersRepository _callForPapersRepository;
+        private readonly IMessageBroker _messageBroker;
+
+        public OpenCallForPapersHandler(
+            ICallForPapersRepository callForPapersRepository,
+            IMessageBroker messageBroker
+        )
+        {
+            _callForPapersRepository = callForPapersRepository;
+            _messageBroker = messageBroker;
+        }
+
+        public async Task HandleAsync(OpenCallForPapers command)
+        {
+            var callForPaper =
+                await _callForPapersRepository.GetAsync(command.ConferenceId)
+                ?? throw new CallForPapersNotFoundException(command.ConferenceId);
+            
+            callForPaper.Open();
+            await _callForPapersRepository.UpdateAsync(callForPaper);
+            await _messageBroker.PublishAsync(
+                new CallForPapersOpened(
+                    callForPaper.ConferenceId,
+                    callForPaper.From,
+                    callForPaper.To
+                )
+            );
+        }
+    }
+}

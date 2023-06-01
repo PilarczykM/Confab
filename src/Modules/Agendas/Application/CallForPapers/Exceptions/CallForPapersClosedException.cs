29ed3d7c15dc1d51343c77b6e4a893af9731d696
PilarczykM <marcinpilarczyk@me.com>
2023-06-01 16:27:40 +0200

    Create CallForPapersClosedException.cs

---
+++ src/Modules/Agendas/Application/CallForPapers/Exceptions/CallForPapersClosedException.cs
@@ -0,0 +1,15 @@
+﻿using Confab.Shared.Abstractions.Exceptions;
+
+namespace Confab.Modules.Agendas.Application.CallForPapers.Exceptions
+{
+    public class CallForPapersClosedException : ConfabException
+    {
+        public CallForPapersClosedException(Guid conferenceId) : base($"Conference with ID: '{conferenceId}' has closed CFP.")
+        {
+            ConferenceId = conferenceId;
+        }
+
+        public Guid ConferenceId { get; }
+    }
+}
+

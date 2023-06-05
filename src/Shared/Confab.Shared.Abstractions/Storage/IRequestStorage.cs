7a0da72e921971275e1e60aa6258fb4cc76a8f92
PilarczykM <marcinpilarczyk@me.com>
2023-06-05 12:01:26 +0200

    Create IRequestStorage.cs

---
+++ src/Shared/Confab.Shared.Abstractions/Storage/IRequestStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace Confab.Shared.Abstractions.Storage
+{
+    public interface IRequestStorage
+    {
+        void Set<T>(string key, T value, TimeSpan? duration = null);
+        T Get<T>(string key);
+    }
+}
+

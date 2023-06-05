aabfd1b413f2abb66d7672a70239810118aae50e
PilarczykM <marcinpilarczyk@me.com>
2023-06-05 15:18:01 +0200

    Create AgendasApiClients.cs

---
+++ src/Modules/Attendances/Infrastructure/Clients/AgendasApiClients.cs
@@ -0,0 +1,28 @@
+﻿using Confab.Modules.Attendances.Application.Clients.Agendas;
+using Confab.Modules.Attendances.Application.Clients.Agendas.DTO;
+using Confab.Modules.Attendances.Infrastructure.Clients.Requests;
+using Confab.Shared.Abstractions.Module;
+
+namespace Confab.Modules.Attendances.Infrastructure.Clients
+{
+    public class AgendasApiClients : IAgendasApiClient
+    {
+        private readonly IModuleClient _moduleClient;
+
+        public AgendasApiClients(IModuleClient moduleClient)
+        {
+            _moduleClient = moduleClient;
+        }
+
+        public Task<IEnumerable<AgendaTrackDto>> GetAgendaAsync(Guid conferenceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RegularAgendaSlotDto> GetRegularAgendaSlotAsync(Guid id) =>
+            _moduleClient.SendAsync<RegularAgendaSlotDto>(
+                "/agendas/slots/reqular/get",
+                new GetRegularAgendaSlot { AgendaItemId = id }
+            );
+    }
+}

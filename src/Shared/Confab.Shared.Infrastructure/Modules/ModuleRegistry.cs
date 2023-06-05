66145f392f2bd40d1fc0b92a5b3d4838f04156b1
PilarczykM <marcinpilarczyk@me.com>
2023-06-05 15:07:16 +0200

    Update ModuleRegistry.cs
    Implement missing members

--- src/Shared/Confab.Shared.Infrastructure/Modules/ModuleRegistry.cs
+++ src/Shared/Confab.Shared.Infrastructure/Modules/ModuleRegistry.cs
@@ -3,6 +3,8 @@
     public class ModuleRegistry : IModuleRegistry
     {
         private readonly List<ModuleBroadcastRegistration> _broadcastRegistrations = new();
+        private readonly Dictionary<string, ModuleRequestRegistration> _requestRegistrations =
+            new();
 
         public void AddBroadcastAction(Type requestType, Func<object, Task> action)
         {
@@ -15,7 +17,27 @@
             _broadcastRegistrations.Add(registration);
         }
 
+        public void AddRequestAction(
+            string path,
+            Type requestType,
+            Type responseType,
+            Func<object, Task<object>> action
+        )
+        {
+            if (path is null)
+            {
+                throw new InvalidOperationException("Request path cannot be null");
+            }
+
+            var registraction = new ModuleRequestRegistration(requestType, responseType, action);
+
+            _requestRegistrations.TryAdd(path, registraction);
+        }
+
         public IEnumerable<ModuleBroadcastRegistration> GetBroadcastRegistrations(string key) =>
             _broadcastRegistrations.Where(x => x.Key == key);
+
+        public ModuleRequestRegistration GetRequestRegistration(string path) =>
+            _requestRegistrations.TryGetValue(path, out var registration) ? registration : null;
     }
 }

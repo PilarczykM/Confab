07dab1cfd471ce45e6a15871283a62c9330ea463
PilarczykM <marcinpilarczyk@me.com>
2023-06-05 15:18:13 +0200

    Create Extensions.cs
    Register IAgendasApiClient

---
+++ src/Modules/Attendances/Infrastructure/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using Confab.Modules.Attendances.Application.Clients.Agendas;
+using Confab.Modules.Attendances.Infrastructure.Clients;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Confab.Modules.Attendances.Infrastructure
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddSingleton<IAgendasApiClient, AgendasApiClients>();
+
+            return services;
+        }
+    }
+}

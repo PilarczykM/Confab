35da1e3a179ba381e4e0b25bfaa3ff06c73a397d
PilarczykM <marcinpilarczyk@me.com>
2023-06-05 15:34:44 +0200

    Register into container

--- src/Modules/Attendances/Infrastructure/Extensions.cs
+++ src/Modules/Attendances/Infrastructure/Extensions.cs
@@ -1,5 +1,9 @@
 ﻿using Confab.Modules.Attendances.Application.Clients.Agendas;
+using Confab.Modules.Attendances.Domain.Repositories;
 using Confab.Modules.Attendances.Infrastructure.Clients;
+using Confab.Modules.Attendances.Infrastructure.EF;
+using Confab.Modules.Attendances.Infrastructure.EF.Repositories;
+using Confab.Shared.Infrastructure.Postgres;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Confab.Modules.Attendances.Infrastructure
@@ -9,6 +13,9 @@ namespace Confab.Modules.Attendances.Infrastructure
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
             services.AddSingleton<IAgendasApiClient, AgendasApiClients>();
+            services.AddScoped<IAttendableEventsRepository, AttendableEventsRepository>();
+            services.AddScoped<IParticipantsRepository, ParticipantsRepository>();
+            services.AddPostgres<AttendancesDbContext>();
 
             return services;
         }

b5ab732409ef6837702db27465c4ef6132edd515
PilarczykM <marcinpilarczyk@me.com>
2023-06-05 11:10:15 +0200

    Add exceptions

---
+++ src/Modules/Agendas/Domain/Agendas/Exceptions/ConflictingAgendaSlotsException.cs
@@ -0,0 +1,17 @@
+﻿using Confab.Shared.Abstractions.Exceptions;
+
+namespace Confab.Modules.Agendas.Domain.Agendas.Exceptions
+{
+    public class ConflictingAgendaSlotsException : ConfabException
+    {
+        public ConflictingAgendaSlotsException(DateTime from, DateTime to) : base($"There is slot clonflicting with date range: {from} | {to}")
+        {
+            From = from;
+            To = to;
+        }
+
+        public DateTime From { get; }
+        public DateTime To { get; }
+    }
+}
+
---
+++ src/Modules/Agendas/Domain/Agendas/Exceptions/EmptyAgendaTrackNameException.cs
@@ -0,0 +1,15 @@
+﻿using Confab.Shared.Abstractions.Exceptions;
+
+namespace Confab.Modules.Agendas.Domain.Agendas.Exceptions
+{
+    public class EmptyAgendaTrackNameException : ConfabException
+    {
+        public EmptyAgendaTrackNameException(Guid id) : base($"Agenda track with ID '{id}' defines empty name.")
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; }
+    }
+}
+

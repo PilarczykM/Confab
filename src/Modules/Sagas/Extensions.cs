91693ab3ecdc82f76fda7df57fea805430df727d
PilarczykM <marcinpilarczyk@me.com>
2023-06-06 11:03:16 +0200

    Create Extensions.cs

---
+++ src/Modules/Sagas/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using Chronicle;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Confab.Modules.Sagas
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddSaga(this IServiceCollection services)
+        {
+            services.AddChronicle();
+            return services;
+        }
+    }
+}
+
